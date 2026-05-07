1d26e11734f82ced2235668d293dff34e811eee6
ivomom-ux <ivo.mom@gmail.com>
2026-05-07 12:58:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 
 const CONFIG = {
   // GitHub instellingen
-  GH_OWNER:     'JOUW_GITHUB_GEBRUIKERSNAAM',  // bv. 'ivomom-ux'
-  GH_REPO:      'JOUW_REPOSITORY_NAAM',         // bv. 'songvote-HotStew'
-  GH_TOKEN:     'JOUW_GITHUB_TOKEN',            // bv. 'ghp_abc123...'
+  GH_OWNER:     'ivomom-ux',  // bv. 'ivomom-ux'
+  GH_REPO:      'songvote-HotStew',         // bv. 'songvote-HotStew'
+  GH_TOKEN:     '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 ',            // bv. 'ghp_abc123...'
   GH_DATA_FILE: 'songvote_data.json',           // bestandsnaam in de repo
 
   // Naam van de beheerder (krijgt import/export/gebruikersbeheer)
-  ADMIN_USER:   'Jan',
+  ADMIN_USER:   'Ivo - trombone',
 };
